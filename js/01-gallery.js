2b049f68a4cee642aa22baa90de26e2a608997b7
EmiliaSzczepaniak52 <emiliaszczepaniak52@gmail.com>
2023-03-14 22:54:52 +0100

    all

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const  gallery= document.querySelector(".gallery");
+galleryItems.forEach((item)=> {
+    const galletyItem=`<div class="gallery__item">
+          <a class="gallery__link" href=${item.original}">
+          <img class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"/></img></a></div>`;
+          gallery.insertAdjacentHTML("beforeend", galletyItem);
+
+     gallery.onclick= (event)=>{
+        event.preventDefault();
+        if (event.target.nodeName !=="IMG"){
+            return;
+        }
+        const instance = basicLightbox.create(
+             `
+            <img width="1400" height="900" src="${event.target.dataset.source}">
+             `
+            );
+        instance.show();
+        gallery.addEventListener("keydown", (event)=>{
+            if (event.key === "Escape"){
+                instance.close();
+            }
+        })
+        };
+    
+    });
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
+import simpleLightbox from '.node_modules/simplelightbox';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+let gallery = new simpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function()
+{
+    gallery.captions= "true";
+    gallery.captionSelector= "img";
+    gallery.captionType="attr";
+    gallery.captionsDate="alt";
+    gallery.captionPosition="bottom";
+    gallery.captionDelay= 0;
+
+});
+const lightbox = new simpleLightbox(".gallery a",
+{
+    caption: true,
+    captionsDate:"alt",
+    captionDelay:250,
+});
+
+
+
+
+
+  
